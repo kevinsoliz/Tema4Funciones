6313fca3383b16647bc607ed4dbe291e2f2bc250
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-28 08:01:53 +0200

    Fácil

---
+++ src/Actividad04.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Actividad04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        byte a = sc.nextByte();
+        System.out.println("Introduce un número: ");
+        byte b = sc.nextByte();
+        System.out.println("Máximo: " + maximo(a, b));
+    }
+    static byte maximo(byte a, byte b) {
+        return a > b ? a : b;
+    }
+}
