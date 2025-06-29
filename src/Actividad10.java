56cef349fa1c1475c8ce5aacc03fdd04112b9a0f
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-29 08:55:44 +0200

    Ni multiplicación, ni suma

--- src/Actividad10.java
+++ src/Actividad10.java
@@ -5,10 +5,12 @@ public class Actividad10 {
         Scanner sc = new Scanner(System.in);
 
         int a = Actividad08.pedirValor("a: ", sc);
-        int n = Actividad08.pedirValor("n: ", sc);
+        byte n = Actividad08.pedirValor("n: ", sc);
 
         for (int i = 0; i < n; i++){
-            a *= a;
+            a += a; //Hay un problema aqui, creo la multiplicación no es adecuada, tampoco la suma.
+                    //a = 2 y n = 3 p.e. En la primera iteración a vale 4, en la segunda hace 4 * 4, en la tercera hace 16 * 16
+                    //Con la suma igual. En la primera iteración a vale 4, en la segunda hace 4 + 4, en la tercera hace 8 + 8
         }
         System.out.println("Resultado: " + a);
     }
