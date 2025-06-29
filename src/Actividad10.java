c486700dd6d19c5ce2ac55cba70d6706157e28d8
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-29 08:59:04 +0200

    Ahora sí.

--- src/Actividad10.java
+++ src/Actividad10.java
@@ -4,14 +4,13 @@ public class Actividad10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = Actividad08.pedirValor("a: ", sc);
+        byte a = Actividad08.pedirValor("a: ", sc);
         byte n = Actividad08.pedirValor("n: ", sc);
+        int resultado = 1;
 
         for (int i = 0; i < n; i++){
-            a += a; //Hay un problema aqui, creo la multiplicación no es adecuada, tampoco la suma.
-                    //a = 2 y n = 3 p.e. En la primera iteración a vale 4, en la segunda hace 4 * 4, en la tercera hace 16 * 16
-                    //Con la suma igual. En la primera iteración a vale 4, en la segunda hace 4 + 4, en la tercera hace 8 + 8
+            resultado *= a;
         }
-        System.out.println("Resultado: " + a);
+        System.out.println("Resultado: " + resultado);
     }
 }
