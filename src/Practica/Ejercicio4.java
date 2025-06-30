85472a375d577f635e1281f14bc0bba6c9b0182e
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-30 17:30:42 +0200

    No tengo la más remota idea de cómo solucionar esto

---
+++ src/Practica/Ejercicio4.java
@@ -0,0 +1,21 @@
+package Practica;
+
+import Actividades.Actividad08;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cantidad = Actividad08.pedirValor("Cantidad: ", sc);
+        int minimo = Actividad08.pedirValor("Minimo: ", sc);
+        int maximo = Actividad08.pedirValor("Maximo: ", sc);
+        numsAleaotorios(cantidad, minimo, maximo);
+    }
+    static void numsAleaotorios(int cantidad, int minimo, int maximo){
+        for (int i = 0; i < cantidad; i++){
+            int aleaotorio = (int)(Math.random() * minimo);
+            System.out.print(aleaotorio + " ");
+        }
+    }
+}
